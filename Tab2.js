3cabac3fcdd010d4aa8b0156339316842292975e
PabloHernandez03 <pablo.hernandez6287@alumnos.udg.mx>
2023-11-12 22:55:17 -0600

    tab-2-end tab-3

--- Tab2.js
+++ Tab2.js
@@ -1,35 +1,296 @@
-import { Text, View, StyleSheet, FlatList } from 'react-native'
-import React, { Component } from 'react'
-import { WebView } from 'react-native-webview';
+import React, { Component } from 'react';
+import { View, Text, ScrollView ,StyleSheet, TouchableOpacity, Image, Modal,FlatList} from 'react-native';
+import { horizontalScale, moderateScale, verticalScale } from './Metrics';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export default class Tab2 extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            modalVisible:false,
+            modalVisible2:false,
+        };
+    }
 
-  render() {
+    render() {
+        const imagenes = ["https://i.pravatar.cc/300", "https://i.pravatar.cc/301", "https://i.pravatar.cc/302","https://i.pravatar.cc/303","https://i.pravatar.cc/304"]
+        const preferencias = ["Anime","Rock Latino","Harry Potter","Minecraft","Anime","Rock Latino","Harry Potter","Minecraft"];
+        const detallesRecibida = () =>{
+            this.setState({modalVisible: true})
+        }
 
-    return (
-      
-      <View style={styles.fondo}>
-        
-      </View>
-    )
-  }
+        const cerrarDetallesRecibida = () =>{
+            this.setState({modalVisible: false})
+        }
+
+        const detallesPendiente = () =>{
+          this.setState({modalVisible2: true})
+      }
+
+      const cerrarDetallesPendiente = () =>{
+          this.setState({modalVisible2: false})
+      }
+
+        return (
+            <ScrollView style={styles.enviadas}>
+                <View>
+                    <Text style={styles.enviadasText}>Enviadas "Pendientes"</Text>
+                    <View style={{borderColor:"black", borderWidth:1,borderColor:"#102A68",width:horizontalScale(330),marginLeft:horizontalScale(30),}}/>
+
+                    <View style={styles.perfil}>
+                        <TouchableOpacity style={styles.botonPerfil} onPress={detallesPendiente}>
+                            <Image style={styles.perfilImage} source={{uri: "https://i.pravatar.cc/300"}}/>
+                            <View style={{marginTop:verticalScale(-110),marginLeft:horizontalScale(120)}}>
+                                <Text style={{color:"White",fontSize:moderateScale(20)}}>Dario Carrasco</Text>
+                                <Text style={{color:"red"}}>Ingenieria Industrial</Text>
+                                <Text style={{color:"White"}}>20 Años</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+
+                <View style={styles.recibidas}>
+                    <Text style={styles.recibidasText}>Recibidas</Text>
+                    <View style={{borderColor:"black", borderWidth:1,borderColor:"#CFA50C",width:horizontalScale(330),marginLeft:horizontalScale(30),}}/>
+
+                    <View style={styles.perfil}>
+                        <TouchableOpacity style={styles.botonPerfil} onPress={detallesRecibida}>
+                            <Image style={styles.perfilImage} source={{uri: "https://i.pravatar.cc/301"}}/>
+                            <View style={{marginTop:verticalScale(-110),marginLeft:horizontalScale(120)}}>
+                                <Text style={{color:"White",fontSize:moderateScale(20)}}>Angelina Acebes</Text>
+                                <Text style={{color:"red"}}>Ingenieria en Computacion</Text>
+                                <Text style={{color:"White"}}>20 Años</Text>
+                            </View>
+
+                            <Modal
+                                visible={this.state.modalVisible}
+                                transparent={true}
+                                animationType='slide'
+                            >
+                                <ScrollView style={styles.vistaPerfil}>
+                                    <View style={{borderWidth:2, backgroundColor:"#102A68", width:horizontalScale(350),height:verticalScale(750),borderRadius:40,marginLeft:horizontalScale(20),marginTop:verticalScale(20)}}>
+                                        <TouchableOpacity onPress={cerrarDetallesRecibida} style={styles.btnX}>
+                                        <Icon
+                                            name={'close-outline'}
+                                            size={35}
+                                            color="white"
+                                            />
+                                        </TouchableOpacity>
+                                        <Image style={styles.perfilImageDetalles} source={{uri: "https://i.pravatar.cc/301"}}/>
+
+                                        <View style={{alignItems:"center",}}>
+                                            <Text style={{color:"White",fontSize:moderateScale(40)}}>Angelina Acebes</Text>
+                                            <Text style={{color:"red"}}>Ingenieria en Computacion</Text>
+                                            <Text style={{color:"White"}}>20 Años</Text>
+                                        </View>
+
+                                        <View style={{marginTop:verticalScale(50), width:horizontalScale(310), marginLeft:horizontalScale(18)}}>
+                                            <FlatList
+                                            data={imagenes}
+                                            horizontal={true}
+                                            showsHorizontalScrollIndicator={false}
+                                            keyExtractor={(item)=>item}
+                                            renderItem={({item})=>{
+                                                return(
+                                                    <Image source={{uri: item}} style={styles.imagenes}/> 
+                                                );
+                                            }}
+                                            />
+                                        </View>
+
+                                        <View style={{alignItems:"center",}}>
+                                            <Text style={{color:"White",fontSize:moderateScale(30)}}>Preferencias</Text>
+                                            <View style={styles.app}>
+                                                {preferencias.map((item) => {
+                                                    return (
+                                                        <View style={styles.item}>
+                                                            <Text style={{color:"white", fontSize:moderateScale(10)}}>{item}</Text>
+                                                        </View>
+                                                    );
+                                                })}
+                                            </View>
+                                        </View>
+                                        <View style={{marginTop:verticalScale(100)}}>
+                                            <TouchableOpacity style={styles.aceptar}><Text style={{color:"White",fontSize:moderateScale(20)}}>Aceptar</Text></TouchableOpacity>
+                                            <TouchableOpacity style={styles.rechazar}><Text style={{color:"White",fontSize:moderateScale(20)}}>Rechazar</Text></TouchableOpacity>
+                                        </View>
+                                    </View>
+                                </ScrollView>
+
+                            </Modal>
+
+                            <Modal
+                                visible={this.state.modalVisible2}
+                                transparent={true}
+                                animationType='slide'
+                            >
+                                <ScrollView style={styles.vistaPerfil}>
+                                  <View style={{borderWidth:2, backgroundColor:"#102A68", width:horizontalScale(350),height:verticalScale(700),borderRadius:40,marginLeft:horizontalScale(20),marginTop:verticalScale(20)}}>
+                                        <TouchableOpacity onPress={cerrarDetallesPendiente} style={styles.btnX}>
+                                        <Icon
+                                            name={'close-outline'}
+                                            size={35}
+                                            color="white"
+                                            />
+                                        </TouchableOpacity>
+                                        <Image style={styles.perfilImageDetalles} source={{uri: "https://i.pravatar.cc/300"}}/>
+
+                                        <View style={{alignItems:"center",}}>
+                                            <Text style={{color:"White",fontSize:moderateScale(40)}}>Dario Carrasco</Text>
+                                            <Text style={{color:"red"}}>Ingenieria Industrial</Text>
+                                            <Text style={{color:"White"}}>20 Años</Text>y
+                                        </View>
+
+                                        <View style={{marginTop:verticalScale(50), width:horizontalScale(310), marginLeft:horizontalScale(18)}}>
+                                            <FlatList
+                                            data={imagenes}
+                                            horizontal={true}
+                                            showsHorizontalScrollIndicator={false}
+                                            keyExtractor={(item)=>item}
+                                            renderItem={({item})=>{
+                                                return(
+                                                    <Image source={{uri: item}} style={styles.imagenes}/> 
+                                                );
+                                            }}
+                                            />
+                                        </View>
+
+                                        <View style={{alignItems:"center",}}>
+                                            <Text style={{color:"White",fontSize:moderateScale(30)}}>Preferencias</Text>
+                                            <View style={styles.app}>
+                                                {preferencias.map((item) => {
+                                                    return (
+                                                        <View style={styles.item}>
+                                                            <Text style={{color:"white", fontSize:moderateScale(10)}}>{item}</Text>
+                                                        </View>
+                                                    );
+                                                })}
+                                            </View>
+                                        </View>
+                                    </View>
+                                </ScrollView>
+
+                            </Modal>
+
+                        </TouchableOpacity>
+                    </View>
+                </View>
+
+            </ScrollView> 
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  caja:{
-    width: 320,
-    height: 350,
-    marginLeft: 5,
-  },
+    imagenes:{
+        width: horizontalScale(100),
+        height: verticalScale(100),
+        borderWidth: 1,
+        borderColor: "#193565",
+        marginRight:horizontalScale(5),
 
-  fondo:{
-    width: 350,
-    height: 228,
-    marginTop: 50,
-  },
+        borderRadius:20,
+    },
+    
+    vistaPerfil:{
+        
+    },
+
+    enviadas:{
+        backgroundColor:"white",
+    },
+
+    enviadasText:{
+        fontSize:moderateScale(25),
+        marginLeft:horizontalScale(33),
+        marginTop:verticalScale(20),
+        color:"#102A68",
+    },
+
+    recibidas:{
+    },
+
+    recibidasText:{
+      fontSize:moderateScale(25),
+      marginLeft:horizontalScale(33),
+      marginTop:verticalScale(20),
+      color:"#CFA50C",
+    },
+
+    perfil:{
+        backgroundColor:"#102A68",
+        marginTop:verticalScale(10),
+        marginLeft:horizontalScale(30),
+        width:horizontalScale(330),
+        height:verticalScale(130),
+        borderRadius:20,
+        
+    },
+    perfilImage:{
+        width:horizontalScale(100),
+        height:verticalScale(100), 
+        margin:15,
+        borderRadius:50,
+    },
+
+    perfilImageDetalles:{
+        width:horizontalScale(150),
+        height:verticalScale(150), 
+        marginLeft:horizontalScale(95),
+        borderRadius:110,
+        borderColor:"black",
+        borderWidth:2,
+    },
+
+    botonPerfil:{
+
+    },
+
+    app: {
+      marginLeft: horizontalScale(80),
+      marginHorizontal: "auto",
+      width: horizontalScale(399),
+      flexDirection: "row",
+      flexWrap: "wrap"
+    },
+    item: {
+    flex: 1,
+    minWidth: horizontalScale(80),
+    maxWidth: horizontalScale(80),
+    height: verticalScale(50),
+    justifyContent: "center",
+    alignItems: "center",
+
+    // my visual styles; not important for grid
+    padding: 10,
+    backgroundColor: "#324b76",
+    borderWidth: 1.5,
+    borderColor: "#193565",
+    borderRadius: 5,
+
+    },
+
+
+    aceptar:{
+        backgroundColor:"#32529D",
+        alignItems:"center",
+        width:horizontalScale(150),
+        height:verticalScale(30),
+        marginLeft:horizontalScale(15),
+        borderRadius:20,
+        borderColor:"black",
+        borderWidth:1,
+    },
+
+    rechazar:{
+        backgroundColor:"#BE1619",
+        alignItems:"center",
+        width:horizontalScale(150),
+        height:verticalScale(30),
+        marginTop:verticalScale(-30),
+        marginLeft:horizontalScale(180),
+        borderRadius:20,
+        borderColor:"black",
+        borderWidth:1,
+    },
 })
--- Tab3.js
+++ Tab3.js
@@ -1,78 +1,214 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Image, FlatList } from 'react-native';
-import MenuDrawer from 'react-native-side-drawer';
-
-export default class Tab3 extends Component {
+import { View, Text, ScrollView ,StyleSheet, TouchableOpacity, Image, Modal,FlatList} from 'react-native';
+import { horizontalScale, moderateScale, verticalScale } from './Metrics';
+import Icon from 'react-native-vector-icons/Ionicons';
+export default class Tab2 extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            open: false,
-            dataSource: [],
+            modalVisible:false,
         };
     }
-
-    toggleOpen = () => {
-        this.setState({ open: !this.state.open });
-    };
-
-    componentDidMount(){
-        var xhttp = new XMLHttpRequest();
-        _this=this;
-        xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                // Typical action to be performed when the document is ready:
-                console.log(xhttp.responseText);
-                var Temporal = JSON.parse(xhttp.responseText);
-                _this.setState({dataSource:Temporal})
-            }
-        };
-        xhttp.open("GET", "https://cuceimobile.space/datos.json", true);
-        xhttp.send();
-    }
-    
-    drawerContent = () => {
-    return (
-        <View style={styles.animatedBox}>
-            <TouchableOpacity onPress={this.toggleOpen}>
-                <Text> Cerrar pestaña </Text>
-            </TouchableOpacity>
-        </View>
-        );
-    };
 
     render() {
-        return (          
-            <View style={styles.container}>
-               
-            </View>
+        const imagenes = ["https://i.pravatar.cc/300", "https://i.pravatar.cc/301", "https://i.pravatar.cc/302","https://i.pravatar.cc/303","https://i.pravatar.cc/304"]
+        const preferencias = ["Anime","Rock Latino","Harry Potter","Minecraft","Anime","Rock Latino","Harry Potter","Minecraft"];
+        const detallesRecibida = () =>{
+            this.setState({modalVisible: true})
+        }
+
+        const cerrarDetallesRecibida = () =>{
+            this.setState({modalVisible: false})
+        }
+
+        return (
+            <ScrollView style={styles.enviadas}>
+                <View style={styles.recibidas}>
+                    <View style={styles.perfil}>
+                        <TouchableOpacity style={styles.botonPerfil} onPress={detallesRecibida}>
+                            <Image style={styles.perfilImage} source={require("./Imagenes/cucei.png")}/>
+                            <View style={{marginTop:-110,marginLeft:120}}>
+                                <Text style={{color:"White",fontSize:25}}>Jardin de Rectoria</Text>
+                                <Text style={{color:"red"}}>Lunes 04 de Diciembre</Text>
+                                <Text style={{color:"White"}}>14:00</Text>
+                            </View>
+
+                            <Modal
+                                visible={this.state.modalVisible}
+                                transparent={true}
+                                animationType='slide'
+                            >
+                                <ScrollView style={styles.vistaPerfil}>
+                                
+                                    <View style={{borderWidth:2,backgroundColor:"#102A68", width:350,height:750,borderRadius:40,marginLeft:20,marginTop:20, position:"relative"}}>
+                                        <Image style={styles.imagenCita} source={require("./Imagenes/cucei.png")}/>
+                                        <TouchableOpacity onPress={cerrarDetallesRecibida} style={styles.btnX}>
+                                        <Icon
+                                            name={'close-outline'}
+                                            size={35}
+                                            color="white"
+                                            />
+                                        </TouchableOpacity>
+                                        <Image style={styles.perfilImageDetalles} source={{uri: "https://i.pravatar.cc/300"}}/>
+                                        
+                                        <View style={{alignItems:"center",}}>
+                                            
+                                            <Text style={{color:"White",fontSize:40}}>Jardin de Rectoria</Text>
+                                            <Text style={{color:"White"}}>-Enfrente de la Biblioteca-</Text>
+                                        </View>
+
+                                        <View style={{marginTop:50, width:310, marginLeft:18, alignItems:"center"}}>
+                                            <Text style={{color:"White",fontSize:25}}>Angela Acebes | INCO</Text>
+                                            <Text style={{color:"red"}}>Lunes 04 de Diciembre</Text>
+                                            <View style={{borderColor:"black", borderWidth:1,borderColor:"red",width:250}}/>
+                                            <Text style={{color:"White",fontSize:20}}>Actividades</Text>
+                                        </View>
+
+                                        <View>
+                                            
+                                            <View style={styles.app}>
+                                                <View style={{backgroundColor:"#2764d6", width:100, height:35, alignItems:"center"}}>
+                                                    <Text style={{fontSize:25}}>Platicar</Text>
+                                                </View>
+                                                <View style={{backgroundColor:"#2764d6", width:100, height:35, alignItems:"center", marginTop:-35,marginLeft:110,}}>
+                                                    <Text style={{fontSize:25}}>Comer</Text>
+                                                </View>
+                                                <View style={{backgroundColor:"#2764d6", width:100, height:35, alignItems:"center",marginTop:-35,marginLeft:220,}}>
+                                                    <Text style={{fontSize:25}}>Relajarse</Text>
+                                                </View>
+                                            </View>
+                                        </View>
+                                        <View style={{marginTop:100,alignItems:"center"}}>
+                                            <TouchableOpacity style={styles.rechazar}><Text style={{color:"White",fontSize:20,marginTop:5}}>Cancelar</Text></TouchableOpacity>
+                                            <Text style={{fontSize:15}}>Nota:No podras cancelar faltando 24 Hrs.</Text>
+                                        </View>
+                                    </View>
+                                </ScrollView>
+
+                            </Modal>
+
+                        </TouchableOpacity>
+                    </View>
+                </View>
+
+            </ScrollView> 
         );
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
-      //flex: 1,
-      backgroundColor: "#fff",
-      alignItems: "center",
-      justifyContent: "center",
-      marginTop: 30,
-      zIndex: 0,
+    app:{
+        marginLeft:10,
+        marginTop:20
     },
-    animatedBox: {
-      flex: 1,
-      backgroundColor: "#38C8EC",
-      padding: 10,
+
+    imagenes:{
+        width: 100,
+        height: 100,
+        borderWidth: 1,
+        borderColor: "#193565",
+        marginRight:5,
+
+        borderRadius:20,
     },
-    body: {
-      alignItems: 'center',
-      justifyContent: 'center',
-      width: 50,
-      height: 50,
-      marginLeft: 300,
+
+    imagenCita:{
+        width:350,
+        height:200,
+        borderRadius:40,
     },
-    tabla: {
-        backgroundColor: "gray",
-        with: 40,
-        height: 50,
-    }
-  })
+    
+
+    enviadas:{
+        backgroundColor:"white",
+    },
+
+    enviadasText:{
+        fontSize:25,
+        marginLeft:33,
+        marginTop:20,
+        color:"#102A68",
+    },
+
+    recibidas:{
+    },
+
+    recibidasText:{
+        fontSize:25,
+        marginLeft:33,
+        marginTop:20,
+        color:"#CFA50C",
+    },
+
+    perfil:{
+        backgroundColor:"#102A68",
+        marginTop:30,
+        marginLeft:30,
+        width:330,
+        height:130,
+        borderRadius:20,
+        
+    },
+    perfilImage:{
+        width:100,
+        height:100, 
+        margin:15,
+        borderRadius:50,
+    },
+
+    perfilImageDetalles:{
+        width:150,
+        height:150, 
+        marginLeft:95,
+        marginTop:20,
+        borderRadius:110,
+        borderColor:"black",
+        borderWidth:2,
+        position:"absolute",
+        zIndex:1,
+    },
+
+    botonPerfil:{
+
+    },
+
+    /*item: {
+        flex: 1,
+        minWidth: horizontalScale(80),
+        maxWidth: horizontalScale(80),
+        height: verticalScale(50),
+        justifyContent: "center",
+        alignItems: "center",
+    
+        // my visual styles; not important for grid
+        padding: 10,
+        backgroundColor: "#324b76",
+        borderWidth: 1.5,
+        borderColor: "#193565",
+        borderRadius: 5,
+    
+        }
+*/
+
+    aceptar:{
+        backgroundColor:"#32529D",
+        alignItems:"center",
+        width:150,
+        height:30,
+        marginLeft:15,
+        borderRadius:20,
+        borderColor:"black",
+        borderWidth:1,
+    },
+
+    rechazar:{
+        backgroundColor:"#BE1619",
+        alignItems:"center",
+        width:250,
+        height:40,
+        marginTop:-30,
+        borderRadius:20,
+        borderColor:"black",
+        borderWidth:1,
+    },
+})
